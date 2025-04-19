a2ee2582eca65e853b945268a6269793d4e55de2
AgentSmithZ <98733501+AgentSmithZ@users.noreply.github.com>
2025-04-19 13:31:08 +0500

    Add files via upload

---
+++ Sites/site1/main/main.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+from mainwindow2 import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__ (self, *args, obj=None,  **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+        res = 0
+        res = 1
+        res = 2
+        res = 4
+        
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
---
+++ Sites/site1/src/components/List.vue
@@ -0,0 +1,140 @@
+<template>
+     <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+          <h1>В тренде</h1>
+            <v-sheet
+              min-height="50vh"
+              rounded="lg"
+              style="padding-top: 5px;"
+            >
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+          <h1>Смотрят сейчас</h1>
+            <v-sheet
+              min-height="50vh"
+              rounded="lg"
+              style="padding-top: 5px;"
+            >
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+          <h1>Новинки</h1>
+            <v-sheet
+              min-height="50vh"
+              rounded="lg"
+              style="padding-top: 5px;"
+            >
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            <v-card
+                class="mx-auto deceiver"
+                href="https://github.com/vuetifyjs/vuetify/"
+                max-width="344"
+                prepend-icon="mdi-github"
+                rel="noopener"
+                subtitle="Check out the official repository"
+                target="_blank"
+                title="1"
+            ></v-card>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
+
+<style scoped>
+.deceiver { 
+    margin-bottom: 7px;
+}
+</style>
