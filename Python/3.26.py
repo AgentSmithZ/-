c662934e9324f87897fa37f1c7a945c6f1c38464
AgentSmithZ <98733501+AgentSmithZ@users.noreply.github.com>
2025-04-25 01:26:58 +0300

    Update 3.26.py

--- Python/3.26.py
+++ Python/3.26.py
@@ -1,1 +1,20 @@
+class calc:
+    def __init__(self, x):
+        self.x = x
+    
+    def calculate(self):
+        ever = str(self.x)
+        print(ever)
+        everone = ever[:-2] + ever[2] + ever[1]
+        print(everone)
+        evertwo = ever[1] + ever[0]+ever[2:]
+        print(evertwo)
+        everthree = ever[1] + ever[2:] + ever[0]
+        print(everthree)
+        everfour = ever[2:] + ever[1] + ever[0]
+        print(everfour)
+        everfive = ever[2:] + ever[0] + ever[1]
+        print(everfive)
 
+obj = calc(782)
+obj.calculate()
